1e48ef0c08590550762b8e08d508b611401cea47
aa55865 <47228470+aa55865@users.noreply.github.com>
2019-05-01 17:14:50 -0500

    nearly complete, just need to finish commenting

--- deapSolver.py
+++ deapSolver.py
@@ -1,56 +1,112 @@
-import random
+import random, operator
 import numpy as np
-from datetime import datetime
 from deap import algorithms, base, tools, creator
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 
 
-def deapSolver(designVarDict, objFuncList, popSize, gens, mutPB, cxPB, elites, children, *constraintList):
+def deapSolver(design_var_dict, obj_func_list, obj_func_names=None, norm_facts=None, POPSIZE=1000, GENS=50, MUTPB=None, CXPB=None, SURVIVORS=100, CHILDREN=1000, constraints=None):
+    """
+    This is an optimization solver based on the Distributed Evolutionary Algorithms in Python (DEAP) package. It uses
+    binary representations of design variables in each individual. Given an objective function (or list of objective
+    functions) and constraints, the solver evolves each population of individuals based on the NSGA-II genetic algorithm.
 
-    def funcEval(individual):
+    :param design_var_dict: dictionary of design variables
+    :param obj_func_list: list of anonymous functions that represents the objective(s)
+    :param obj_func_names: optional, specifies function names
+    :param norm_facts: specify normalization factors for objective function outputs. If problem has
+                       multiple objective functions with outputs expected to be on different orders of magnitude, this
+                       is very important to include. Choose normalization factors approximately equal to maximum
+                       expected output from its associated function
+    :param POPSIZE: size of initial population
+    :param GENS: number of generations to be evaluated
+    :param MUTPB: probability of mutation
+    :param CXPB: probability of crossover
+    :param SURVIVORS: number of individuals selected after initial population is evaluated. This governs the size of all
+                      future populations
+    :param CHILDREN: number of offspring to be produced from the current population at each generation
+    :param constraints: list constraints to be applied in evaluating the validity of each individual
+    :return: the solver returns a table of solution points that contains the variable and objective function values for
+             all optimized solutions. A print(results) statement will display the table. For problems with two objective
+             functions, the solver also returns a plot of the Pareto Frontier
+
+    EXAMPLE SETUP FOR SOLVER:
+
+            design_var_dict = {'r': {'interval': [0,10],'bits': 10},'h':{'interval':[0,20],'bits':10}}
+
+            S = lambda r,h: math.pi*r*math.sqrt(r**2+h**2)
+            T = lambda r,h: math.pi*r**2+math.pi*r*math.sqrt(r**2+h**2)
+            obj_func_list = [S,T]
+            obj_func_names = ['Lateral Surface Area','Total Area']
+            norm_facts = [155,225]
+
+            constraints = [[V, '>', 200]] --> [[func(r,h),'operator',val],[func(r,h),'operator',val],etc.]
+
+            gens = 50
+            popSize = 1000
+            mutPB = 0.1
+            cxPB = 0.9
+            survivors = 100
+            children = 1000
+
+            results = deapSolver(design_vars, func_list, obj_func_names=func_names, norm_facts=norm_facts,
+                      POPSIZE=popSize, GENS=gens, MUTPB=mutPB, CXPB=cxPB, SURVIVORS=survivors, CHILDREN=children,
+                      constraints=constraint_list)
+    """
+    # Assign values optional keyword arguments whose length depends on the number of objective functions specified
+    if norm_facts is None:
+        norm_facts = [1 for _ in obj_func_list]
+
+    if MUTPB is None:
+        maxbits = 0
+        for var in design_var_dict:
+            if design_var_dict[var]['bits']>maxbits:
+                maxbits=design_var_dict[var]['bits']
+        MUTPB = 1/maxbits
+
+    if CXPB is None:
+        CXPB  = 1-MUTPB
+
+    if obj_func_names is None:
+        obj_func_names = ['f{}'.format(i+1) for i in range(len(obj_func_list))]
+
+
+    def bi2de_ind(individual): #convert binary individuals to list of decimal variable values
         deciInd = []
         counter = 0
-        for var in designVarDict:
-            xL = designVarDict[var]['interval'][0]
-            xU = designVarDict[var]['interval'][1]
-            diff = xU-xL
-            bits = designVarDict[var]['bits']
-            step = diff/(2**bits-1)
-            binary = ''.join(map(str,individual[counter:(counter+bits)]))
-            deciVal = int(binary,2)
-            deciInd.append(xL+deciVal*step)
-            counter+=bits
-        outputs = [func(*deciInd) for func in objFuncList]
+        for var in design_var_dict:
+            xL = design_var_dict[var]['interval'][0]
+            xU = design_var_dict[var]['interval'][1]
+            diff = xU - xL
+            bits = design_var_dict[var]['bits']
+            step = diff / (2 ** bits - 1)
+            binary = ''.join(map(str, individual[counter:(counter + bits)]))
+            deciVal = int(binary, 2)
+            deciInd.append(xL + deciVal * step)
+            counter += bits
+        return deciInd
+
+    def func_eval(individual): #evaluate individual fitness by plugging variables into objective function(s)
+        deciInd = bi2de_ind(individual)
+        outputs = [func(*deciInd)/norm_fact for func,norm_fact in zip(obj_func_list,norm_facts)]
         return tuple(outputs)
 
-    def feasible(individual):
-        deciInd = []
-        counter = 0
-        deciInd = []
-        counter = 0
-        for var in designVarDict:
-            xL = designVarDict[var]['interval'][0]
-            xU = designVarDict[var]['interval'][1]
-            diff = xU-xL
-            bits = designVarDict[var]['bits']
-            step = diff/(2**bits-1)
-            binary = ''.join(map(str,individual[counter:(counter+bits)]))
-            deciVal = int(binary,2)
-            deciInd.append(xL+deciVal*step)
-            counter+=bits
+    ops = {'>': operator.gt,'<': operator.lt,'>=':operator.ge,'<=':operator.le,'=':operator.eq}
+    def feasibility(individual): #determine if an individual's variables violate any constraints
+        deciInd = bi2de_ind(individual)
+        for constraint in constraints:
+            if ops[constraint[1]](constraint[0](*deciInd),constraint[2]) is False:
+                return False
+        return True
 
-
-    def uniform(designVars): #this function determines the values that fill each individual in the initial population
-        random.seed(datetime.now())
+    def uniform(): #fill each individual in the initial population with total bits in design_var_dict
         bits=0
-        for var in designVars:
-            bits += designVars[var]['bits']
+        for var in design_var_dict:
+            bits += design_var_dict[var]['bits']
         individual = [random.randint(0,1) for _ in range(bits)]
         return individual
 
-    def cxList(ind1, ind2): #define crossover strategy for lists
-        random.seed(datetime.now())
+    def cx_list(ind1, ind2): #define crossover strategy for lists
         cxPt = random.randint(0,len(ind1)-1)
         child1 = toolbox.individual()
         child2 = toolbox.individual()
@@ -58,81 +114,61 @@ def deapSolver(designVarDict, objFuncList, popSize, gens, mutPB, cxPB, elites, c
         child2[::] = ind2[0:cxPt]+ind1[cxPt::]
         return child1,child2
 
-    def mutList(individual): #define mutation strategy for lists
-        random.seed(datetime.now())
+    def mut_list(individual): #define mutation strategy for lists
         mutPoint = random.randint(0,len(individual)-1)
         if individual[mutPoint] == 1: individual[mutPoint] = 0
         else: individual[mutPoint] = 1
         return individual,
 
-    weights = tuple([-1.0 for _ in objFuncList])
+    #intialize all parameters 
+    weights = tuple([-1.0 for _ in obj_func_list])
     creator.create("Fitness", base.Fitness, weights=weights)
     creator.create("Individual", list, fitness=creator.Fitness)
     toolbox = base.Toolbox()
-    toolbox.register("designVar", uniform, designVarDict)
+    toolbox.register("designVar", uniform)
     toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.designVar)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    toolbox.register("mate", cxList)
-    toolbox.register("mutate",mutList)
-    toolbox.register("evaluate", funcEval)
-    if constraintList:
-        toolbox.decorate('evaluate', tools.DeltaPenalty(feasible, 10,000))
+    toolbox.register("mate", cx_list)
+    toolbox.register("mutate",mut_list)
+    toolbox.register("evaluate", func_eval)
+    if constraints:
+        toolbox.decorate("evaluate", tools.DeltaPenality(feasibility,10000))
     toolbox.register("select", tools.selNSGA2)
-    pop = toolbox.population(n=popSize)
-    hof = tools.ParetoFront()
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", np.mean, axis=0)
     stats.register("std", np.std, axis=0)
     stats.register("min", np.min, axis=0)
     stats.register("max", np.max, axis=0)
 
-    stats = algorithms.eaMuPlusLambda(pop, toolbox, elites, children, cxPB, mutPB, gens, stats, halloffame=hof)
-    
-    fits = [ind.fitness.values[:] for ind in stats[0]]
-    
-    headerList = ['Solution'] # generate headers for table
-    for var in designVarDict:
-        headerList.append(var)
-    for i in range(len(objFuncList)):
-        headerList.append("f{0}".format(i+1))
+    pop = toolbox.population(n=POPSIZE)
+    hof = tools.ParetoFront()
+    output = algorithms.eaMuPlusLambda(pop, toolbox, SURVIVORS, CHILDREN, CXPB, MUTPB, GENS, stats, halloffame=hof)
+
+    headerList = ['Solution Point']
+    for var in design_var_dict: headerList.append(var)
+    for name in obj_func_names: headerList.append(name)
     results = PrettyTable(headerList)
-    #print(results)
-    solution=1
-    #print(solutionList)
-    for individual in stats[0]: # generate rows of values for table
+
+    solution = 1
+    for individual in output[0]:
         solutionList = [solution]
-        counter = 0
-        
-        for var in designVarDict:
-            xL = designVarDict[var]['interval'][0]
-            xU = designVarDict[var]['interval'][1]
-            diff = xU-xL
-            bits = designVarDict[var]['bits']
-            step = diff/(2**bits-1)
-            binary = ''.join(map(str,individual[counter:(counter+bits)]))
-            deciVal = xL + int(binary,2)*step
-            #print(deciVal)
-            solutionList.append(deciVal)
-            #print(solutionList)
-        #print(solutionList)
-        #print(solutionList)
-        for fit in individual.fitness.values:
-            solutionList.append(fit)
+        deciInd = bi2de_ind(individual)
+        for val in deciInd:
+            solutionList.append(val)
+        for val,norm_fact in zip(individual.fitness.values,norm_facts):
+            solutionList.append(val*norm_fact)
         results.add_row(solutionList)
         solution+=1
-    
-    print(results)
 
-    if len(objFuncList) == 2 :
-        non_dom = tools.sortNondominated(stats[0], k=len(stats[0]), first_front_only=True)[0]
+    if len(obj_func_list) == 2 :
+        non_dom = tools.sortNondominated(output[0], k=len(output[0]), first_front_only=True)[0]
         for ind in non_dom:
-            plt.plot(*ind.fitness.values, 'bo')
+            fitvals = [ind.fitness.value*norm_fact for ind.fitness.value,norm_fact in zip(ind.fitness.values,norm_facts)]
+            plt.plot(*fitvals,'bo')
         plt.title('Pareto Front')
-        plt.xlabel('f1')
-        plt.ylabel('f2')
+        plt.xlabel('{}'.format(obj_func_names[0]))
+        plt.ylabel('{}'.format(obj_func_names[1]))
         plt.show()
-        return stats
+        return results
     else:
-        return stats
-
-
+        return results
